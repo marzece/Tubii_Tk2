2e97a1c5af9059a00d2eb8d67c966eea3807013d
Eric M <marzece@physics.upenn.edu>
2015-03-07 23:39:19 -0500

    Made some rooms and layed them out

--- temp/packagerxl.bat
+++ temp/packagerxl.bat
@@ -1,4 +1,3 @@
 echo off
 netassembler
-concept2cm -forward -export -proj "C:\Users\Eric Marzec\Documents\Tubii_Tk2\tubii_tk2.cpm"
 pxl.exe -proj "C:\Users\Eric Marzec\Documents\Tubii_Tk2\tubii_tk2.cpm" -nonetassembler
